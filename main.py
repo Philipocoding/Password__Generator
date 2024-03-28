33e6b234562884a6cf072ba08d78a9f6f7c83eae
Philipocoding <philipgraham_1@hotmail.co.uk>
2024-03-28 17:14:40 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import string
+import random
+
+length = int(input("How long do you want your password to be - "))
+
+CharsList = string.ascii_letters
+NumsList = string.digits
+
+password = ""
+for x in range (length):
+    randomPosition = random.randint(0, len(CharsList) - 1)
+    Uppercase = random.randint(0,1)
+    if Uppercase == 1:
+        radomNum = random.randint(0, len(NumsList) -1)
+        y = CharsList[randomPosition]
+        uppercase_char = y.upper()
+        password = password + y + str(radomNum)
+    else:
+        password = password + CharsList[randomPosition]
+
+
+print(password)
